27b1a44a9ef9e696e5e2abec14c363f3eb13d31d
Tej-Birring <tejbirring@gmail.com>
2021-01-23 22:36:25 +0000

    re-enable 'disable context menus'

--- src/use/disableContextMenus.js
+++ src/use/disableContextMenus.js
@@ -2,12 +2,12 @@ import { onMounted } from "vue";
 
 export default function useDisableContextMenus() {
     onMounted( () => {
-        // document.addEventListener("contextmenu", (ev) => {
-        //     // console.log(ev);
-        //
-        //     // disable context menus on images:
-        //     if (ev.target.tagName === "IMG")
-        //         ev.preventDefault();
-        // });
+        document.addEventListener("contextmenu", (ev) => {
+            // console.log(ev);
+
+            // disable context menus on images:
+            if (ev.target.tagName === "IMG")
+                ev.preventDefault();
+        });
     });
 }
